8205de08bc33b16004db28e68e960f3ec92a4365
NatiErco <165024507+NatiErco@users.noreply.github.com>
2024-07-05 10:45:39 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { React } from 'react'
+import { Counter } from './Counter'
+import { Clock } from './Clock'
+import { MouseClicker } from './MouseClicker'
+import { MyForm } from './MyForm'
+import { InteractiveWelcome } from './InteractiveWelcome'
+import { Welcome } from './Welcome'
+import { Login } from './Login'
+import { UncontrolledLogin } from './UncontrolledLogin'
 
-export const App = () => {
+
+export function App () {
+  function handleLogin(loginData) {
+    console.log("Login data:", loginData);
+  }
+
   return (
-    <div>App</div>
+    <div>
+
+
+     <br/> <br/> <br/>
+     <InteractiveWelcome/>
+     <Login onLogin={handleLogin}/> <br/> <br/> <br/>
+     <UncontrolledLogin onLogin={handleLogin}/>
+    </div>
+
+    // <Counter initialValue={0} increment={1} /> Podria usar asi sino le agisno valor a las prop en Counter.jsx
   )
 }
+
 
