b5d9c4aa6286c5c9fc027f555ced7b0c0567fb26
NatiErco <165024507+NatiErco@users.noreply.github.com>
2024-07-09 01:12:58 +0200

    Add files via upload

---
+++ src/Components/Colors.jsx
@@ -0,0 +1,19 @@
+// Cree un segundo componente llamado Colors, que tome una matriz de objetos como propiedad, donde las propiedades siempre sean "name" e "id", y muestre una lista desordenada de componentes Color.
+
+import React from 'react';
+import { Color } from './Color';
+
+
+// Componente Colors
+export function Colors ({ colors }) {
+
+    // Mapea sobre la matriz y para cada objeto crea un componente Color, pasando el objeto color como propiedad.
+  return (
+    <ul>
+      {colors.map(color => (
+        <Color key={color.id} color={color} /> // Devuelve una lista desordenada (ul) que contiene múltiples componentes Color.
+      ))}
+    </ul>
+  );
+};
+
