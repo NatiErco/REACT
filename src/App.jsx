652fcd03be3293d3250c5cc1218bf3839f706471
NatiErco <165024507+NatiErco@users.noreply.github.com>
2024-07-09 01:14:23 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { React } from "react";
+import { Color } from "./Components/Color";
+import { Colors } from "./Components/Colors";
 
-export const App = () => {
+export function App() {
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <Colors
+        colors={[
+          { id: 1, name: "Red" },
+          { id: 2, name: "Yellow" },
+          { id: 3, name: "White" },
+        ]}
+      />
+    </div>
+  );
 }
-
