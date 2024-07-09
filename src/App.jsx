49bafcc27d15cbbfc4b3bed8140e0820bb33f8c5
NatiErco <165024507+NatiErco@users.noreply.github.com>
2024-07-09 16:01:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,6 @@ export function App() {
 
   return (
     <Container title={<h1>My title Aplication</h1>}>
-      <Colors
-        colors={[
-          { id: 1, name: "Red" },
-          { id: 2, name: "Yellow" },
-          { id: 3, name: "White" },
-        ]}
-      />
        </Container>
   );
 }
