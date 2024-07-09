a02a747abfde973288f31694b0efa78da62fa604
NatiErco <165024507+NatiErco@users.noreply.github.com>
2024-07-09 02:28:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { React } from "react";
-import { Color } from "./Components/Color";
-import { Colors } from "./Components/Colors";
+import { Welcome } from "./Components/Welcome";
 
 export function App() {
 
   return (
     <div>
-      <Colors
-        colors={[
-          { id: 1, name: "Red" },
-          { id: 2, name: "Yellow" },
-          { id: 3, name: "White" },
-        ]}
-      />
+ <Welcome/>
     </div>
   );
 }
