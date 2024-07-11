f1e135f46548ff90304f7afbd88bb54897638698
Natalia Ercolano <165024507+NatiErco@users.noreply.github.com>
2024-07-11 02:31:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { React } from "react";
-import { Container } from "./Components/Container";
+import { TodoList } from "./Components/TodoList";
 
 export function App() {
 
   return (
-    <Container title={<h1>My title Aplication</h1>}>
-       </Container>
+    
+       <TodoList/>
   );
 }
