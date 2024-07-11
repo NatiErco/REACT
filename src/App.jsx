52822ef0a5be2f584603ad63b1f278d824b68db7
Natalia Ercolano <165024507+NatiErco@users.noreply.github.com>
2024-07-11 21:30:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,1 @@
-import { React } from "react";
-import { Container } from "./Components/Container";
 
-export function App() {
-
-  return (
-    <Container title={<h1>My title Aplication</h1>}>
-       </Container>
-  );
-}
