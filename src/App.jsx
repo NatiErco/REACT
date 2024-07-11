33ddaa123ddc740897dcd1d44968b33589ef8712
Natalia Ercolano <165024507+NatiErco@users.noreply.github.com>
2024-07-11 20:45:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { React } from "react";
-import { Container } from "./Components/Container";
+import { React, useState } from "react";
+import { LanguageContext } from "./Components/LanguageContext";
+import { ClockII } from "./Components/ClockII";
 
 export function App() {
 
+  // const lenguages = ["english", "spanish", "german", "japanese", "italian"]; Otra opcion era usar este array y luego con metodo map 
+
+  const [select, setSelect] = useState("spanish");
+
+  function handleSelect(event) {
+    setSelect(event.target.value);
+  }
+
+  console.log(select);
   return (
-    <Container title={<h1>My title Aplication</h1>}>
-       </Container>
+    <div>
+      <form>
+        <select value={select} onChange={handleSelect}>
+          <option value="english">english</option>
+          <option value="spanish">spanish</option>
+          <option value="german">german</option>
+        </select>
+      </form>
+
+      <LanguageContext.Provider value={select}>
+        <ClockII />
+      </LanguageContext.Provider>
+    </div>
   );
 }
