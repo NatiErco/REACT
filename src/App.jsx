3289106c1b70f7ff00a9097e80e92022ecbd5eb9
Natalia Ercolano <165024507+NatiErco@users.noreply.github.com>
2024-07-12 10:35:34 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import { React, useState } from "react";
+import { GithubUser } from "./Components/GithubUser";
 
+
+
+export function App() {
+  return (
+    
+    <div>
+
+    <GithubUser/>
+    
+    </div>
+  );
+}
