5778895c575789d32019f178279c8a61f80dc316
Natalia Ercolano <165024507+NatiErco@users.noreply.github.com>
2024-07-12 12:04:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import { React } from "react";
-import { Container } from "./Components/Container";
+import { React, useState } from "react";
+import { Welcome } from "./Components/Welcome";
+import { Route, Routes } from "react-router-dom";
+import { GithubUser } from "./Components/GithubUser";
+import { GithubUsers } from "./Components/GithubUsers";
+import { Counter } from "./Components/Counter";
+import { ShowGithubUse } from "./Components/ShowGithubUse";
+
+
+// Podemos usar React Router para definir rutas y renderizar diferentes componentes cada vez que cambie la ubicacion en la barra de direcciones.
 
 export function App() {
-
   return (
-    <Container title={<h1>My title Aplication</h1>}>
-       </Container>
+    <div>
+    <div><GithubUser username="NatiErco"/></div>
+      <Routes>
+      <Route path="/" element={<Welcome/>}/>
+        <Route path="/:name" element={<Welcome/>}/>
+        <Route path="/" element={<Counter/>}/>
+       <Route path="/:username" element={<ShowGithubUse/>}/>
+      </Routes>
+    </div>
   );
 }
+
