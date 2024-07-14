81690e77bd5f44fa0bcb62ff61db71a5287c6c41
Natalia Ercolano <165024507+NatiErco@users.noreply.github.com>
2024-07-14 15:03:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { React } from "react";
-import { Container } from "./Components/Container";
+import { Welcome } from "./Components/Welcome";
+import { Link, Route, Routes } from "react-router-dom";
+import { Catalogue } from "./Components/Catalogue";
+import { Products } from "./Components/Products";
+
+// Podemos usar React Router para definir rutas y renderizar diferentes componentes cada vez que cambie la ubicacion en la barra de direcciones.
 
 export function App() {
-
   return (
-    <Container title={<h1>My title Aplication</h1>}>
-       </Container>
+    <div title={<h1>My Awesome App</h1>}>
+      <div>
+        <Link to="/">Home</Link> |  <Link to="/products">Products</Link>
+        
+      </div>
+      <Routes>
+        <Route path="/" element={<Welcome/>} />
+        <Route path="products" element={<Catalogue />} />
+        <Route path=":id" element={<Products/>}/>
+      </Routes>
+    </div>
+  
   );
 }
