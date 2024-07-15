011ab05d2109956ddaa5fee129abd5912fa9ce39
Natalia Ercolano <165024507+NatiErco@users.noreply.github.com>
2024-07-15 10:27:32 +0200

    Add files via upload

---
+++ src/Components/UseGithubUser.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+
+export function UseGithubUser ({username}) {
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+
+      // Modificar el useGithubUsergancho para que, si el nombre de usuario es nulo, no se realice ninguna solicitud.
+
+      if (!username) {
+        setData(null);
+        setLoading(false);  // si username es nulo, simplemente no se ejecutará la solicitud.
+        return;
+      }
+
+      setLoading(true);
+      fetch('https://api.github.com/users')
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return res.json();
+        })
+        .then((json) => setData(json))
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
+    }, [username]);
+
+
+    return {
+        data,
+        loading,
+        error,
+    };
+}
